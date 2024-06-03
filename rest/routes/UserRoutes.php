f2e1e67a5d9f2f598c193f97e506245e8c541f03
merjjjem <merjem.ahmedspahic@stu.ibu.edu.ba>
2024-06-03 23:57:02 +0200

    Final Commit

---
+++ rest/routes/UserRoutes.php
@@ -0,0 +1,52 @@
+<?php
+
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+
+
+
+/**
+* @OA\Post(
+* path="/login",
+* description="Login",
+* tags={"login"},
+* @OA\RequestBody(description="Login", required=true,
+* @OA\MediaType(mediaType="application/json",
+* @OA\Schema(
+* @OA\Property(property="email", type="string", example="demo@gmail.com", description="Admin email" ),
+* @OA\Property(property="password", type="string", example="12345", description="Password" ),
+* )
+* )),
+* @OA\Response(
+* response=200,
+* description="Logged in successfuly"
+* ),
+* @OA\Response(
+* response=500,
+* description="Error"
+* )
+* )
+*/
+Flight::route('POST /login', function(){
+$login = Flight::request()->data->getData();
+$user = Flight::userDao()->get_user_by_email($login['email']);
+if(count($user) > 0){
+$user = $user[0];
+}
+if (isset($user['id'])){
+if($user['password'] == md5($login['password'])){
+unset($user['password']);
+$user['is_admin'] = false;
+$jwt = JWT::encode($user, Config::JWT_SECRET(), 'HS256');
+Flight::json(['token' => $jwt]);
+}else{
+Flight::json(["message" => "Wrong password"], 404);
+}
+}else{
+Flight::json(["message" => "User doesn't exist"], 404);
+}
+});
+
+
+
+?>
---
+++ rest/services/UserService.php
@@ -0,0 +1,15 @@
+
+
+<?php
+require_once 'BaseService.php';
+require_once __DIR__."/../dao/UserDao.class.php";
+class UserService extends BaseService{
+public function __construct(){
+parent::__construct(new UserDao());
+}
+public function add($entity){
+return parent::add($entity);
+
+}
+}
+?>
